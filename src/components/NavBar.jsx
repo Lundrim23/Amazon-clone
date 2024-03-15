e0ceb43570320bed17eeb08589330f1a3c6478cd
Lundrim <aliulundrim22@gmail.com>
2024-03-15 07:29:05 +0100

    Styling for Product Page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ShoppingCartIcon } from '@heroicons/react/24/outline';
 import Search from './Search';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
@@ -8,11 +9,15 @@ function NavBar() {
       {/* Left */}
       <div className="flex bg-amazon text-white h-[60px] ">
         <div className="flex items-center m-4">
-          <img
-            className="h-[35px] w-[100px] m-2"
-            src={'../images/amazon.png'}
-            alt="logo"
-          />
+          <Link to={'/'}>
+            {' '}
+            <img
+              className="h-[35px] w-[100px] m-2"
+              src={'../images/amazon.png'}
+              alt="logo"
+            />
+          </Link>
+
           <div className="pr-4 pl-4">
             <div className="text-xs xl:text-sm">Deliver to </div>
             <div className="text-sm xl:text-base font-bold">United State</div>
@@ -34,10 +39,12 @@ function NavBar() {
             <div className="text-xs xl:text-sm">Returns</div>
             <div className="text-sm xl:text-base font-bold">& Orders</div>
           </div>
-          <div className="flex pr-3 pl-3">
-            <ShoppingCartIcon className="h-[48px]"></ShoppingCartIcon>
-            <div className="mt-7 text-xs xl:text-sm font-bold">Cart</div>
-          </div>
+          <Link to={'/checkout'}>
+            <div className="flex pr-3 pl-3">
+              <ShoppingCartIcon className="h-[48px]"></ShoppingCartIcon>
+              <div className="mt-7 text-xs xl:text-sm font-bold">Cart</div>
+            </div>
+          </Link>
         </div>
       </div>
       <div className="flex bg-amazon-light_blue text-white space-x-3 text-xs xl:text-sm p-2 pl-6 ">
