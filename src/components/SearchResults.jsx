f20ce9e6e3db8c63823caee03c94a8b94e84b88f
Lundrim <aliulundrim22@gmail.com>
2024-03-15 08:07:22 +0100

    Seach Functionality Finished

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+import { callAPI } from './utils/CallApi';
 
 function SearchResults() {
-  return <div>SearchResults</div>;
+  const [searchParams] = useSearchParams();
+  const [products, setProducts] = useState(null);
+
+  const getSearchResults = () => {
+    const searchTerm = searchParams.get('searchTerm');
+    const category = searchParams.get('category');
+
+    callAPI(`data/search.json`).then((searchResults) => {
+      const categoryResult = searchResults[category];
+      if (searchTerm) {
+        const results = categoryResult.filter(
+          (product) =>
+            product.title.toLowerCase().includes(searchTerm.toLowerCase()) 
+        );
+        setProducts(results);
+      } else {
+        setProducts(categoryResult);
+      }
+    });
+  };
+
+  useEffect(() => {
+    getSearchResults(); 
+  }, [searchParams]);
+
+  return (
+    <div className="min-w-[1200px] max-w-[1300px] m-auto">
+      {' '}
+      
+      {products &&
+        products.map((product, key) => {
+          return <div key={key}> {product.title} </div>;
+        })}
+    </div>
+  );
 }
 
 export default SearchResults;
