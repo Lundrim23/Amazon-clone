831324bfa698b29f1d754ca8b1795aa79aaad747
Lundrim <aliulundrim22@gmail.com>
2024-03-15 08:37:25 +0100

    Carousel Category Updated

--- src/components/CarouselCategory.jsx
+++ src/components/CarouselCategory.jsx
@@ -1,9 +1,22 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import { useNavigate, createSearchParams } from 'react-router-dom';
+
 import 'swiper/css';
 import 'swiper/css/navigation';
-import { Navigation } from 'swiper/modules';
 
-function CarousalCategory() {
+const CarouselCategory = () => {
+  const navigate = useNavigate();
+  const searchCategory = (category) => {
+    navigate({
+      pathname: '/search',
+      search: `${createSearchParams({
+        category: `${category}`,
+        searchTerm: ``,
+      })}`,
+    });
+  };
+
   return (
     <div className="bg-white m-3">
       <div className="text-2xl font-semibold p-3">Shop by Category</div>
@@ -13,27 +26,45 @@ function CarousalCategory() {
         navigation={true}
         modules={[Navigation]}
       >
-        <SwiperSlide>
-          <img src={'../images/category_0.jpg'} alt="Category" />
+        <SwiperSlide
+          onClick={() => searchCategory('Deals')}
+          className="cursor-pointer mt-[20px]"
+        >
+          <img src={'../images/category_0.jpg'} alt="Deal category" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={'../images/category_1.jpg'} alt="Category" />
+        <SwiperSlide
+          onClick={() => searchCategory('Amazon')}
+          className="cursor-pointer"
+        >
+          <img src={'../images/category_1.jpg'} alt="Amazon category" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={'../images/category_2.jpg'} alt="Category" />
+        <SwiperSlide
+          onClick={() => searchCategory('Fashion')}
+          className="cursor-pointer"
+        >
+          <img src={'../images/category_2.jpg'} alt="Fashion category" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={'../images/category_3.jpg'} alt="Category" />
+        <SwiperSlide
+          onClick={() => searchCategory('Computers')}
+          className="cursor-pointer"
+        >
+          <img src={'../images/category_3.jpg'} alt="Computers category" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={'../images/category_4.jpg'} alt="Category" />
+        <SwiperSlide
+          onClick={() => searchCategory('Home')}
+          className="cursor-pointer"
+        >
+          <img src={'../images/category_4.jpg'} alt="Home category" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={'../images/category_5.jpg'} alt="Category" />
+        <SwiperSlide
+          onClick={() => searchCategory('Mobiles')}
+          className="cursor-pointer"
+        >
+          <img src={'../images/category_5.jpg'} alt="Mobiles category" />
         </SwiperSlide>
       </Swiper>
     </div>
   );
-}
+};
 
-export default CarousalCategory;
+export default CarouselCategory;
